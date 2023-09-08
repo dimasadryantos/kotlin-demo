da612e8276b2b7206f8ffd04decf18b15712919c
dsetyawan <dimas.adriyanto@ratepay.com>
2023-09-08 21:54:03 +0200

    Const and nulls

---
+++ src/main/kotlin/com/dzcorp/demokotlin/Const.kt
@@ -0,0 +1,11 @@
+package com.dzcorp.demokotlin
+
+
+const val NAME = 3.14 //same as static final in java
+fun main() {
+    val name: String = "Dimas" //Immutable - same as final in java - read only
+
+    var name2: String = "Dimas2" //mutable
+
+    //Const is known before code execution whereas final is after code execution
+}
---
+++ src/main/kotlin/com/dzcorp/demokotlin/Nulls.kt
@@ -0,0 +1,15 @@
+package com.dzcorp.demokotlin
+
+fun main(){
+
+
+    var name: String?= "DIMAS"
+    name = null
+
+    // ? is null safety in kotlin it means allowing variable to have null value
+
+
+
+
+
+}
